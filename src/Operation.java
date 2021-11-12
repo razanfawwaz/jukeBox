bfbe0c794f61380259b2f72e80456860513a3254
TsaniAflah <tsaniaflah6@gmail.com>
2021-11-13 00:04:56 +0700

    ClassOperation
    boolean back to main menu

--- src/Operation.java
+++ src/Operation.java
@@ -28,13 +28,14 @@ public class Operation
     {
         Scanner menu = new Scanner(System.in);
         boolean condition = true;
+        Operation classOperation = new Operation();
         while (condition)
         {
             System.out.println("\n\t \t \t \t \t \t \t\t\t\t   JUKEBOX \t \t \t \t\t \t \t ");
-            System.out.println(("____________________________________________________________________________________________________"));
+            System.out.println(("_____________________________________________________________________________________________________"));
             System.out.print("|\t ");
             System.out.println("SONG NAME \t \t \t \t \t\tARTIST \t \t \t \t \t\t ALBUM \t \t \t\t \t DURATION \t|");
-            System.out.println("____________________________________________________________________________________________________");
+            System.out.println("|___________________________________________________________________________________________________|");
             System.out.println("|(1)Traitor\t\t\t\t\t\tOlivia Rodrigo\t\t\t\tSour\t\t\t\t\t\t\t3.50\t|");
             System.out.println("|(2)Good 4 U\t\t\t\t\tOlivia Rodrigo\t\t\t\tSour\t\t\t\t\t\t\t2.59\t|");
             System.out.println("|(3)Happier Than Ever\t\t\tBillie Eilish\t\t\t\tHappier Than Ever\t\t\t\t4.59\t|");
@@ -57,9 +58,51 @@ public class Operation
                 case "1" ->
                     {
                         System.out.println("\n\t \t \t \t \t \t \t\t\t\t   PLAY MUSIC \t \t \t \t\t \t \t ");
-                        System.out.println(("____________________________________________________________________________________________________"));
+                        System.out.println("_____________________________________________________________________________________________________");
+                        System.out.print("|\t ");
+                        System.out.println("SONG NAME \t \t \t \t \t\tARTIST \t \t \t \t \t\t ALBUM \t \t \t\t \t DURATION \t|");
+                        System.out.println("|___________________________________________________________________________________________________|");
+                        System.out.println("|(1)Traitor\t\t\t\t\t\tOlivia Rodrigo\t\t\t\tSour\t\t\t\t\t\t\t3.50\t|");
+                        System.out.println("|(2)Good 4 U\t\t\t\t\tOlivia Rodrigo\t\t\t\tSour\t\t\t\t\t\t\t2.59\t|");
+                        System.out.println("|(3)Happier Than Ever\t\t\tBillie Eilish\t\t\t\tHappier Than Ever\t\t\t\t4.59\t|");
+                        System.out.println("|(4)Bad Habits\t\t\t\t\tEd Sheeran\t\t\t\t\tBad Habits \t\t\t\t\t\t3.52\t|");
+                        System.out.println("|(5)Painkiller\t\t\t\t\tRuel \t\t\t\t\t\tPainkiller\t\t\t\t\t\t3.34 \t|");
+                        System.out.println("|(6)Cheap Thrills\t\t\t\tSia\t\t\t\t\t\t\tThis is Acting\t\t\t\t\t3.45\t|");
+                        System.out.println("|(7)Wish U Were Here\t\t\tAvril Lavigne\t\t\t\tGoodbye Lullaby\t\t\t\t\t3.46\t|");
+                        System.out.println("|(8)Womanizer\t\t\t\t\tBritney Spears \t\t\t\tCircus\t\t\t\t\t\t\t3.45\t|");
+                        System.out.println("|(9)Rumah ke Rumah\t\t\t\tHindia \t\t\t\t\t\tMenari dengan bayangan\t\t\t4.38\t|");
+                        System.out.println("|(10)Lara\t\t\t\t\t\tDialog Senja \t\t\t\tLara\t\t\t\t\t\t\t5.27\t|");
+                        System.out.println("|___________________________________________________________________________________________________|");
+                        //System.out.print("  |\t \tPUT INPUT NUMBER : "); String option = menu.nextLine();
+                        System.out.println("|___________________________________________________________________________________________________|");
+
                         return 1;
                     }
+                    case "2" ->
+                    {
+                        System.out.println("\n\t \t \t \t \t \t \t\t\t\t   PLAYLIST  \t \t \t \t\t \t \t ");
+                        System.out.println("____________________________________________________________________________________________________");
+                    }default -> System.out.println("INVALID INPUT, TRY AGAIN");
+            }System.out.println("\n\t================BACK TO MAIN MENU================");
+            System.out.println("TYPE 'Y' FOR YES");
+            System.out.println("TYPE 'N' FOR QUIT");
+            System.out.println("\t======================================================");
+
+            if(statement.equalsIgnoreCase("y"))
+            {
+                classOperation.setStart("Y");
+            }else
+            {
+                classOperation.setStart("N");
+            }
+
+            if (classOperation.isStart())
+            {
+                condition = true;
+            }else
+            {
+                condition = false;
+                break;
             }
         }
         return 0;
