0d1b234e793403d6da341ceea3494c25bdd59ed6
razanfawwaz <razanfawwaz1905@gmail.com>
2021-11-12 19:59:50 +0700

    add JukeBox class and PlayList class

---
+++ src/JukeBox.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class JukeBox {
+    List<Song> allSongs;
+    List<PlayList> allPlayLists;
+    Queue<Song> currentPlayQueue;
+
+    public JukeBox(List<Song> allSongs,List<PlayList> allPlayLists) {
+        this.allSongs = allSongs;
+        this.allPlayLists = allPlayLists;
+        currentPlayQueue = new LinkedList<Song>();
+    }
+
+    public Song selectSong(String songName){
+        Song requiredSong = null;
+        for(Song song : allSongs){
+            if(song.getSongName().equals(songName)){
+                requiredSong = song;
+                break;
+            }
+        }
+        if(requiredSong == null){
+            throw new IllegalArgumentException("Sorry, song not available");
+        }
+        return requiredSong;
+    }
+
+    public void playSong(String songName){
+        Song requiredSong = selectSong(songName);
+        requiredSong.playSong();
+    }
+
+    public void queueNextSong(String songName){
+        Song requiredSong = selectSong(songName);
+        currentPlayQueue.add(requiredSong);
+    }
+
+    public PlayList selectPlayList(String playListName){
+        PlayList requiredPlayList = null;
+        for(PlayList playList : allPlayLists){
+            if(playList.getPlaylistName().equals(playListName)){
+                requiredPlayList = playList;
+            }
+        }
+        if(requiredPlayList == null){
+            throw new IllegalArgumentException("Sorry, playlist not available");
+        }
+        return requiredPlayList;
+    }
+
+    public void playPlayList(String playListName){
+        PlayList playList = selectPlayList(playListName);
+        List<Song> allSongsInThePlayList = playList.getSongsInPlaylist();
+        allSongsInThePlayList.get(0).playSong();
+
+        Song currentSong;
+        for(int i = 1; i < allSongsInThePlayList.size(); i++){
+            currentSong = allSongsInThePlayList.get(i);
+            currentPlayQueue.add(currentSong);
+        }
+    }
+}
---
+++ src/PlayList.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.lang.String;
+
+public class PlayList {
+    String playlistName;
+    List<Song> songsInPlaylist;
+
+    public PlayList (String playlistName, List<Song> songsInPlaylist){
+        this.playlistName = playlistName;
+        this.songsInPlaylist = songsInPlaylist;
+    }
+
+    public void addSong(Song song){
+        songsInPlaylist.add(song);
+    }
+
+    public String getPlaylistName(){
+        return playlistName;
+    }
+
+    public List<Song> getSongsInPlaylist(){
+        return songsInPlaylist;
+    }
+}
